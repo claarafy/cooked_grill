e9f343fc318ebfa0df0278a1e3557e16d50318d9
Avan <mujibsardar@gmail.com>
2018-03-14 18:40:19 -0700

    Fixed top nav a little, aligned to the right

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # config.time_zone = 'Pacific Time (US & Canada)'
-  before_action :set_user_time_zone
+  before_action :set_user_time_zone, :index
   $timeFormat = "%b %e, %l:%M %p %Z"
+
+  def index
+    @contact = Contact.new
+  end
 
   def set_user_time_zone
     Time.zone = 'Pacific Time (US & Canada)'
@@ -12,7 +16,7 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user!, :except => [:index]
 
   # Temp for HTTP Verification
-  # TODO remove later. Not needed anymore. 
+  # TODO remove later. Not needed anymore.
   def download_file
     send_file 'app/assets/videos/B7C7017DAF6B1432EB6A73124F6494CC.txt'
   end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,16 @@
 class HomeController < ApplicationController
   def index
     @meals = Meal.where("availability_to > ?", Time.zone.now.beginning_of_day).where(active: true)
-    @contact = Contact.new
+    if !@contact
+      @contact = Contact.new
+    end
   end
 
   def subscribe_create
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = 'Thank you. You have been subscribed!'
+      flash.now[:notice_subscribe] = 'Thank you. You have been subscribed!'
       @meals = Meal.where("availability_to > ?", Time.zone.now.beginning_of_day).where(active: true)
       render :index
     else
