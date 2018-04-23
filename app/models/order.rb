83d7f50dddb3d240d2ae5d3dc3ca34b212715f06
Avan <mujibsardar@gmail.com>
2018-04-23 09:26:57 -0700

    Removed create new delivery migration file, add cooks first name to admin meals

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,4 @@ class Order < ApplicationRecord
   belongs_to :meal
   has_one :cook, through: :meal
   belongs_to :review
-  has_one :new_delivery_address
-  accepts_nested_attributes_for :new_delivery_address
 end
