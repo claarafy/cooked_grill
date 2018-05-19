ccdaaa101fe1f431f3990536baa05d23acac2ead
Avan <mujibsardar@gmail.com>
2018-05-19 11:11:55 -0700

    Implementing cook sign up on landing page...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def resource_name
+    :cook
+  end
+
+  def resource
+    @resource ||= Cook.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:cook]
+  end
 end
